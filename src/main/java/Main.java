40d75cbbccc3b69961f5bd73a4889eba3cfa90ea
Ailin Ghoreishi <aghoreishi2004@gmail.com>
2024-03-06 22:29:31 +0330

    main evolved

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        // TODO --> complete main function
+        System.out.println("Welcome. Please choose:");
+        System.out.println("Movie");
+        System.out.println("Celebrity");
         runMenu();
     }
     public static void runMenu() {
-        // TODO
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        if(str == "Movie"){
+            Scanner scanner1 = new Scanner(System.in);
+            String str1 = scanner1.next();
+            Movie Item = new Movie(new ArrayList<>(),"",0);
+        }
+
+        if(str == "Celebrity"){
+            Scanner scanner1 = new Scanner(System.in);
+            String str1 = scanner1.next();
+            Actors Item = new Actors("",true);
+        }
     }
 }
